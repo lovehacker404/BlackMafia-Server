33d5ca2d0b42e8cfde3e05eef794826d057fd4fe
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-12-03 15:05:55 +0500

    Update Android-Setup.sh

--- Android-Setup.sh
+++ Android-Setup.sh
@@ -6,7 +6,7 @@ echo ""
 echo ""
 echo ""
 echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m!!! Ngrok Installation Plz Wi8 !!! \e[0m'
-sleep 2
+sleep 3
 clear
 echo ""
 echo ""
@@ -66,9 +66,11 @@ cp -R ngrok BlackMafiaLovehacker Sites/pinterest/
 chmod 7777 BlackMafia.sh
 clear
 echo ""
-echo -e $'\e[1;91m[\e[0m\e[1;33m ⛔ \e[0m\e[1;96m]\e[0m\e[1;92m ⛔Enter The Ngrok Token  \e[1;33m[ *** ]\e[0m'
+echo -e $'\e[1;91m[\e[0m\e[1;33m ⛔ \e[0m\e[1;96m]\e[0m\e[1;92m 01⛔  https://ngrok.com  ⛔\e[0m'
+echo -e $'\e[1;91m[\e[0m\e[1;35m ⛔ \e[0m\e[1;96m]\e[0m\e[1;92m 02⛔ Login Ngrok WebSite ⛔\e[0m'
+echo -e $'\e[1;91m[\e[0m\e[1;32m ⛔ \e[0m\e[1;96m]\e[0m\e[1;92m 93⛔   Copy Ngrok Token  ⛔\e[0m'
 echo ""
-read -p $'\e[1;91m[\e[0m\e[1;77m⛔\e[0m\e[1;91m]\e[0m\e[1;95m [➡️https://ngrok.com/⬅️\e[0m\e[1;92mlogin & Copy Token Paste here➡️] : \e[0m' token
+read -p $'\e[1;91m[\e[0m\e[1;71m ⛔ \e[0m\e[1;91m]\e[0m\e[1;95m Enter Token ➡️ : \e[0m' token
 $token
 sleep2
 clear
